d9430b43a0c00d3111f3f1a04ad5fdb0d3cf33eb
JorgeGarciaLeon <78152033+JorgeGarciaLeon@users.noreply.github.com>
2021-11-30 13:59:09 +0000

    Poniendo tiempos menores a los productores.
    Feature: Añadimos tiempos cortos de producción, muestra como se producen antes de que el consumidor intente consumirlo.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ public class Main {
     public static void main(String[] args) {
         VegetablePatch vegetables = new VegetablePatch(5);
 
-        Farmer farmer1 = new Farmer(vegetables, 3, 5000);
-        Farmer farmer2 = new Farmer(vegetables, 2, 5000);
+        Farmer farmer1 = new Farmer(vegetables, 3, 1000);
+        Farmer farmer2 = new Farmer(vegetables, 2, 1000);
 
-        Customer customer1 = new Customer(vegetables,3, 3000);
-        Customer customer2 = new Customer(vegetables,2, 3000);
+        Customer customer1 = new Customer(vegetables,3, 6000);
+        Customer customer2 = new Customer(vegetables,2, 6000);
 
-        farmer1.setPriority(9);
-        farmer2.setPriority(9);
+
         farmer1.start();
         farmer2.start();
         customer1.start();
